a82696276438be2c9aad08ed5b524b5f3639d102
Jeff Allen <jeff.allen@trestletechnology.net>
2013-02-13 16:33:50 -0600

    Update mpiStartENA.sh
    Commented out library reference.

--- mpiStartENA.sh
+++ mpiStartENA.sh
@@ -1,5 +1,6 @@
 #job should be started with -d switch pointing to the proper dir. For instance:
 # qsub -l nodes=4:ppn=12 mpiStartENA.sh -d `pwd`
 
-export R_LIBS_USER="/home/jalle6/R/library"
+#You may want to update your R library environment variable to point to wherever you store your R packages. For instance:
+#export R_LIBS_USER="/home/jalle6/R/library"
 /opt/openmpiv2/bin/mpirun --hostfile $PBS_NODEFILE -n 1 Rscript -e "library(knitr); knit2html(\"index.Rmd\");"
